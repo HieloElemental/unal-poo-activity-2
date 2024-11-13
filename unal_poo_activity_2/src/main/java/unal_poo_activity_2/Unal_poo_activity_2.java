063627f26b15385540fe7306e53e40df27c7a22e
HieloElemental <hieloelemental@gmail.com>
2024-11-13 17:36:35 -0500

    feat(template): basic template with activity title and author ID.

---
+++ unal_poo_activity_2/src/main/java/unal_poo_activity_2/Unal_poo_activity_2.java
@@ -0,0 +1,16 @@
+/*
+ * Ejercico Propuesto No 
+ */
+
+package unal_poo_activity_2;
+
+/**
+ *
+ * @author 1022003484
+ */
+public class Unal_poo_activity_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
