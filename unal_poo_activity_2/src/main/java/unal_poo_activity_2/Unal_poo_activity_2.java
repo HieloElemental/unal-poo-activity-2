03749ab775dc48c0ceec19c6e09428471f7d10cb
HieloElemental <hieloelemental@gmail.com>
2024-11-13 18:17:55 -0500

    feat(template): add scanner to basic template.

--- unal_poo_activity_2/src/main/java/unal_poo_activity_2/Unal_poo_activity_2.java
+++ unal_poo_activity_2/src/main/java/unal_poo_activity_2/Unal_poo_activity_2.java
@@ -4,11 +4,14 @@
 
 package unal_poo_activity_2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author 1022003484
  */
 public class Unal_poo_activity_2 {
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
