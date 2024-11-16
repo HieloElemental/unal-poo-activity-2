c85a25b6b0a1ee32ae533352eb89009d7225b08c
HieloElemental <hieloelemental@gmail.com>
2024-11-15 21:33:47 -0500

    feat: add full chapter 4 proposed exercise 7.

--- unal_poo_activity_2/src/main/java/unal_poo_activity_2/Unal_poo_activity_2.java
+++ unal_poo_activity_2/src/main/java/unal_poo_activity_2/Unal_poo_activity_2.java
@@ -1,5 +1,5 @@
 /*
- * Ejercico Propuesto No 
+ * Cap 4 - Ejercico Resuelto No 7
  */
 
 package unal_poo_activity_2;
@@ -14,6 +14,22 @@ public class Unal_poo_activity_2 {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        double numberA = getDouble("insert number A");
+        double numberB = getDouble("insert number B");
+        
+        if(numberA > numberB){
+            System.out.println("A is greater than B");
+        } else {
+            if(numberA == numberB) {
+                System.out.println("A is equal than B");
+            } else {
+                System.out.println("A is lesser than B");
+            }
+        }
+    }
+    
+    public static double getDouble(String message) {
+        System.out.print(message + ": ");
+        return scanner.nextDouble();
     }
 }
