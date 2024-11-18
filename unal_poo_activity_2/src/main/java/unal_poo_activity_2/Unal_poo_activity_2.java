7789e98134bf2b1ab79b967f671e6562135bc74c
HieloElemental <hieloelemental@gmail.com>
2024-11-18 12:37:33 -0500

    feat: add full chapter 4 proposed exercise 11.

--- unal_poo_activity_2/src/main/java/unal_poo_activity_2/Unal_poo_activity_2.java
+++ unal_poo_activity_2/src/main/java/unal_poo_activity_2/Unal_poo_activity_2.java
@@ -1,5 +1,5 @@
 /*
- * Ejercico Propuesto No 
+ * Cap 4 - Ejercico Resuelto No 11 
  */
 
 package unal_poo_activity_2;
@@ -14,6 +14,26 @@ public class Unal_poo_activity_2 {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        double[] numbers = new double[3];
+        for(int i = 0; i < numbers.length; i++) {
+            numbers[i] = getDouble("insert number " + String.valueOf(i + 1));
+        }
+        
+        double maxValue = Math.max(Math.max(numbers[0], numbers[1]), numbers[2]);
+        
+        System.out.print("El valor mayor entre " + 
+                String.valueOf(numbers[0]) + 
+                ", " +
+                String.valueOf(numbers[1]) +
+                " y " + 
+                String.valueOf(numbers[2]) + 
+                " es " +
+                String.valueOf(maxValue)
+        );
+    }
+    
+    public static double getDouble(String message) {
+        System.out.print(message + ": ");
+        return scanner.nextDouble();
     }
 }
